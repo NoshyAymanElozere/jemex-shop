bd2531b286185436376a1ca2f19e45a7c44c8257
Yousef Ahmed <112353240+Yousef767@users.noreply.github.com>
2024-01-09 21:46:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-let nav = document.querySelector('nav');
-let menu = document.querySelector('.menu');
+let nav = document.querySelector("nav");
+let menu = document.querySelector(".menu");
+menu.addEventListener("click", () => {
+  nav.classList.toggle("activeMenu");
+});
+let acc = document.querySelector(".account");
+let drop = document.querySelectorAll(".drop");
 
-menu.addEventListener('click',()=>{
-  nav.classList.toggle('activeMenu');
-})
+
+drop.forEach((e) => {
+  e.addEventListener("click", () => {
+    acc.classList.remove("showAcc");
+    drop.forEach((el) => {
+      el !== e ? el.classList.remove("showMenu") : "";
+    });
+    e.classList.toggle("showMenu");
+  });
+});
+
+acc.addEventListener("click", () => {
+  drop.forEach((el) => {
+    el.classList.remove("showMenu");
+  });
+  acc.classList.toggle("showAcc");
+});
